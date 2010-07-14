a3a6113b1efd7a8b6cad4bf5a48ff5271607264a
Kla <kla.haeck@gmail.com>
2010-07-14 16:01:49 -0500

    added models and migration files for CKEditor file uploads.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100712230249) do
+ActiveRecord::Schema.define(:version => 20100714205537) do
 
   create_table "applicants", :force => true do |t|
     t.integer  "program_id"
@@ -24,6 +24,22 @@ ActiveRecord::Schema.define(:version => 20100712230249) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "assets", :force => true do |t|
+    t.string   "data_file_name"
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    :limit => 25
+    t.string   "type",              :limit => 25
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "assets", ["assetable_id", "assetable_type", "type"], :name => "ndx_type_assetable"
+  add_index "assets", ["assetable_id", "assetable_type"], :name => "fk_assets"
+  add_index "assets", ["user_id"], :name => "fk_user"
 
   create_table "jurorships", :force => true do |t|
     t.integer  "program_id"
