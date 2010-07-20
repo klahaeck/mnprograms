e1d34524d66d397df8f64acca8d49c29e708de91
Kla <kla.haeck@gmail.com>
2010-07-20 10:16:21 -0500

    added work types to seeded data for initial db migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,12 @@
 puts 'SETTING UP DEFAULT USER LOGIN'
 user = User.create! :username => 'admin', :email => 'admin@root.com', :password => '9487oy', :password_confirmation => '9487oy', :role => 'admin'
 puts 'New user created: ' << user.email
+
+type1 = Type.create! :title => 'Visual Arts'
+puts 'New type created: ' << type1.title
+type2 = Type.create! :title => 'Music'
+puts 'New type created: ' << type2.title
+type3 = Type.create! :title => 'Performance/Video'
+puts 'New type created: ' << type3.title
+type4 = Type.create! :title => 'Literary'
+puts 'New type created: ' << type4.title
